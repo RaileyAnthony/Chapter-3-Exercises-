f48c58a90783e8c9844d7182ba17e1c4885c5ccf
Ruily123 <80341106+Ruily123@users.noreply.github.com>
2022-09-27 18:51:53 +0400

    Update chap3ex4.py

--- chap3ex4.py
+++ chap3ex4.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1sCHrRVPkjaSs5KHiv_T-aXR6Gp-wsZo3
 """
 
-Guests = ["Dyan","Nami", "Rami"]
+Guests = ["Dyan", "Nami", "Rami"]
 message = ("Hello " + Guests[0] + ", you are invited to the dinner.")
 message2 = ("Hello " + Guests[1] + ", you are invited to the dinner.")
 message3 = ("Hello " + Guests[2] + ", you are invited to the dinner.")
